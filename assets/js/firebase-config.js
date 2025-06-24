611bbdb7b093bc561878be5d26b1627ede63c627
Tarsis Borges <tarsisn70@gmail.com>
2025-06-24 19:29:32 -0300

    Implementação de login com Firebase

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "
+AIzaSyBanidBJY8CpsmyAiEEuuXRYE2Hl8sS-uc",
+  authDomain: "https://confec-o-qbfr.vercel.app/index.html",
+  projectId: "
+fluxo-inteligente",
+};
+firebase.initializeApp(firebaseConfig);
